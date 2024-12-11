ec92f4e4cc635058182c41051552ba1438ffbd46
zaindgr8 <66069515+zaindgr8@users.noreply.github.com>
2024-12-11 22:08:40 +0400

    1008

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,17 @@
+"use client"
 import React from "react";
 import SEO from "../common/seo";
 import HomeThree from "../components/homes/home-3";
 import Wrapper from "../layout/wrapper";
+import Popup from "../components/pop_up/pop_up";
 
 const Home = () => {
+
   return (
     <Wrapper>
       <SEO pageTitle={"DEVMATE"} />
-       <HomeThree />
+      <HomeThree />
+      <Popup />
     </Wrapper>
   );
 };
